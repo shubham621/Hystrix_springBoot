278aaaf641a7d1eb5d52ebfac800038591a79c9a
shubham621 <shubhamkumar.kumar11@gmail.com>
2019-09-18 19:29:28 +0530

    School served the logic of cu=ircuit breaker

---
+++ Student_hystrix/src/main/java/com/hystrix/example/StudentHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.hystrix.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHystrixApplication.class, args);
+	}
+
+}
